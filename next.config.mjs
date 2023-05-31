4d1fea1a03f0794e680de7909305f3f35bb44ac7
WJG <wjg12341234@163.com>
2023-05-31 21:52:50 +0800

    misc: remove markdown support

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,9 @@
-import NextMDX from '@next/mdx';
-import rehypeHighlight from 'rehype-highlight';
-import rehypeKatex from 'rehype-katex';
-import { remarkHeadingId } from 'remark-custom-heading-id';
-import remarkGfm from 'remark-gfm';
-import remarkMath from 'remark-math';
-
-const withMDX = NextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkHeadingId, remarkGfm, remarkMath],
-    rehypePlugins: [rehypeHighlight, rehypeKatex],
-  },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx'],
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
